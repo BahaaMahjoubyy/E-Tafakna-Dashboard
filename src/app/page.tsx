3a1fb7c98b81f7265b7c36e92650c4806ee0668c
BahaaMahjoubyy <bmahjuby@gmail.com>
2024-09-09 09:06:59 +0100

    Full-Task

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+const Homepage = () => {
+  return (
+    <Link href="/sign-in" > Homepage </Link>
+  )
+}
+
+export default Homepage
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+const LoginPage = () => {
+  return (
+    <Link href="/admin" > sign-in </Link>
+  )
+}
+
+export default LoginPage
